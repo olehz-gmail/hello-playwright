729ecf7e3ee0de3e6cdb76be5695c6cf372b6a1a
ozabolotnyy <ozabolotnyy@gmail.com>
2026-01-03 23:35:42 +0200

    added 2 tests

--- tests/udemy/test-google-page.spec.ts
+++ tests/udemy/test-google-page.spec.ts
@@ -2,15 +2,42 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Assignments on the google page', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('https://www.google.com/');
-    await expect(page).toHaveTitle(/Google/);
+    await page.goto('https://playwright.dev/docs/intro');
+    await expect(page).toHaveTitle(/Playwright/);
+
+    await page.getByRole('button', { name: 'Search (Ctrl+K)' }).click();
+    await expect(page.getByRole('searchbox', { name: 'Search' })).toBeVisible();
   });
 
-  test('Verify visibility of Google Search button', async ({ page }) => {
-    await page.getByRole('combobox', { name: 'Пошук' }).click();
-    await page.getByRole('combobox', { name: 'Пошук' }).fill('way2automation');
-    await page.getByRole('button', { name: 'Пошук Google' }).first().click();
-    const searchItems = await page.locator('[data-rpos]:has(h3)').all();
-    console.log(`Number of search results: ${searchItems.length}`);
+  test('Walk through the Search results directly', async ({ page }) => {
+    await page.getByRole('searchbox', { name: 'Search' }).fill('input');
+    const suggestions = await page.locator('#docsearch-list > li a');
+    await expect(suggestions.last()).toBeVisible();
+    const suggestionsCount = await suggestions.count();
+    await expect(suggestionsCount).toBeGreaterThan(5);
+
+    for (const element of await suggestions.filter({ hasText: /(:?Input|Handle)/g }).all()) {
+      const text = await element.innerText();
+      const url = await element.getAttribute('href');
+      console.log(`${text} --- ${url}`);
+    }
+  });
+
+  test('Validate Search suggestions', async ({ page }) => {
+    await page.getByRole('searchbox', { name: 'Search' }).fill('getting');
+    const sections = await page.locator('section:has(.DocSearch-Hit-source)');
+    await expect(await sections.last()).toBeVisible();
+    await expect(await sections.count()).toBeGreaterThan(2);
+    for (const section of await sections.all()) {
+      const sectionTitle = await section.locator('.DocSearch-Hit-source').innerText();
+      console.log(`Section: ${sectionTitle}`);
+      const suggestions = await section.locator('li.DocSearch-Hit');
+      await expect(suggestions.last()).toBeVisible();
+      for (const suggestion of await suggestions.all()) {
+        const text = await suggestion.innerText();
+        const url = await suggestion.locator('a').getAttribute('href');
+        console.log(`  ${text} --- ${url}`);
+      }
+    }
   });
 });
